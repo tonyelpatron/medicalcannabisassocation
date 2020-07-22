2d86e12b4bbcc3df028cb662f63fa4872cb91db6
Roderick De Groen <roderickdegroen@MacBook-Pro-van-Roderick.local>
2020-07-22 16:35:43 +0200

    patch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -173,9 +173,120 @@ const Navbar = class extends React.Component {
               </div>
             </div>
         </div>
+        
       </div>
+      
 </div>
+
               
+<div className="navbar-item is-hoverable is-mega" to="/products">
+                
+
+                <Link className="navbar-item has-dropdown is-hoverable is-mega" to="/products">
+                    Knowledge base
+                  </Link>
+    
+                    <div id="blogDropdown" class="navbar-dropdown " data-style="width: 18rem;">
+              <div class="container is-fluid">
+                <div class="columns">
+                  <div class="column">
+                    <h1 class="title is-6 is-mega-menu-title">Learn about CBD Oil</h1>
+                    <a class="navbar-item" href="/2017/08/03/list-of-tags/">
+                      <div class="navbar-content">
+    
+                        
+                        <p>What is CBD Oil?</p>
+                        <p>Studies on CBD oil</p>
+                       <p>How to recognize good CBD oil</p>
+                        <p>CBD oil buying guide</p>
+    
+    
+                      </div>
+                    </a>
+                    
+                  
+                   
+                  </div>
+                  <div class="column">
+                    <h1 class="title is-6 is-mega-menu-title">Learn about CBD Cosmetics</h1>
+                    <a class="navbar-item" href="/2017/08/03/list-of-tags/">
+                      <div class="navbar-content">
+    
+                        
+                        <p>What are CBD Cosmetics?</p>
+                        <p>Studies on CBD cosmetics</p>
+                        <p>How to recognize good CBD cosmetics</p>
+                        <p>CBD cosmetics buying guide</p>
+    
+                      </div>
+                    </a>
+                
+                  </div>
+                  <div class="column">
+                    <h1 class="title is-6 is-mega-menu-title">Learn about CBD Topicals</h1>
+                     <a class="navbar-item" href="/2017/08/03/list-of-tags/">
+                      <div class="navbar-content">
+    
+                        
+                        <p>What are CBD Topicals?</p>
+                        <p>Studies on CBD Topicals?</p>
+                        <p>How to recognize good CBD Topicals?</p>
+                        <p>CBD Topicals buying guide</p>
+    
+                      </div>
+                    </a>
+                  
+    
+                  </div>
+                  <div class="column">
+                    <h1 class="title is-6 is-mega-menu-title">Learn about CBD Food</h1>
+                    <a class="navbar-item" href="/2017/08/03/list-of-tags/">
+                      <div class="navbar-content">
+    
+                        
+                      <p>What is CBD food?</p>
+                        <p>Studies on CBD food</p>
+                        <p>How to recognize good quality CBD food</p>
+                        <p>CBD food buying guide</p>
+    
+                        
+                        
+                                          </div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+    
+                
+    
+                  
+    
+              <hr class="navbar-divider"/>
+              <div class="navbar-item">
+                <div class="navbar-content">
+                  <div class="level is-mobile">
+                    <div class="level-left">
+                      <div class="level-item">
+                        <strong>Stay up to date!</strong>
+                      </div>
+                    </div>
+                    <div class="level-right">
+                      <div class="level-item">
+                        <a class="button bd-is-rss is-small" href="http://bulma.io/atom.xml">
+                          <span class="icon is-small">
+                            <i class="fa fa-rss"></i>
+                          </span>
+                          <span>Subscribe</span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            
+          </div>
+          
+    </div>
               
              
               <Link className="navbar-item" to="/about">
@@ -190,11 +301,7 @@ const Navbar = class extends React.Component {
                 News
               </Link>
 
-              <Link className="navbar-item" to="/news">
-                Shop
-              </Link>
-
-              <Link className="navbar-item" to="/contact">
+              <Link className="navbar-item has-dropdown" to="/contact">
                 Contact
               </Link>
              
